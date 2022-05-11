a7398faf696bd4dd155ff3ce363970a3cec5e3a9
giovanni-pennone <59024132+giovanni-pennone@users.noreply.github.com>
2022-05-11 17:50:01 +0200

    prima versione consensi

---
+++ sansolconsensi/src/components/global/icons/CsiIconAvatarDoctor.vue
@@ -0,0 +1,34 @@
+<!--
+  - SPDX-License-Identifier: EUPL-1.2
+  - Copyright Regione Piemonte - 2022
+  -->
+
+<style scoped></style>
+
+
+<template>
+  <component :is="iconComponent" />
+</template>
+
+
+<script>
+
+  import CsiIconAvatarDoctorMale from "components/global/icons/CsiIconAvatarDoctorMale";
+  import CsiIconAvatarDoctorFemale from "components/global/icons/CsiIconAvatarDoctorFemale";
+
+  export default {
+    name: "CsiIconAvatarDoctor",
+    props: {
+      isFemale: {type: Boolean, required: false, default: false}
+
+    },
+    computed: {
+      iconComponent() {
+        if (!this.isFemale) return CsiIconAvatarDoctorMale
+        if (this.isFemale) return CsiIconAvatarDoctorFemale
+
+        return CsiIconAvatarDoctor
+      },
+    }
+  }
+</script>
---
+++ sansolconsensi/src/components/global/icons/CsiIconStructureGroup.vue
@@ -0,0 +1,95 @@
+<!--
+  - SPDX-License-Identifier: EUPL-1.2
+  - Copyright Regione Piemonte - 2022
+  -->
+
+<template functional>
+  <g class="csi-icon-structure-group">
+    <g transform="translate(-333.719 -206.326)">
+      <g transform="translate(334.257 211.549)">
+        <rect class="a" width="9.158" height="17.708"/>
+      </g>
+      <g transform="translate(359.291 211.549)">
+        <rect class="a" width="9.158" height="17.708"/>
+      </g>
+      <g transform="translate(342.939 206.861)">
+        <rect class="b" width="16.639" height="22.397"/>
+      </g>
+      <g transform="translate(333.719 206.326)">
+        <path class="c"
+              d="M350.12,206.326h-6.708a.752.752,0,0,0-.751.751v3.354a.367.367,0,0,1-.367.366H334.47a.752.752,0,0,0-.751.751v17.887a.752.752,0,0,0,.751.751h33.536a.752.752,0,0,0,.752-.751V211.548a.752.752,0,0,0-.752-.751h-7.825a.367.367,0,0,1-.367-.366v-3.354a.752.752,0,0,0-.751-.751H350.12Zm-7.458,21.991a.367.367,0,0,1-.367.367h-6.708a.367.367,0,0,1-.366-.367v-15.65a.367.367,0,0,1,.366-.367h6.708a.367.367,0,0,1,.367.367Zm15.65,0a.367.367,0,0,1-.367.367h-1.117a.367.367,0,0,1-.367-.367v-3.353a.751.751,0,0,0-1.5,0v3.353a.367.367,0,0,1-.367.367h-6.708a.367.367,0,0,1-.367-.367v-3.353a.751.751,0,0,0-1.5,0v3.353a.367.367,0,0,1-.366.367h-1.118a.367.367,0,0,1-.367-.367V208.2a.367.367,0,0,1,.367-.367h13.414a.367.367,0,0,1,.367.367Zm8.943,0a.367.367,0,0,1-.366.367h-6.708a.367.367,0,0,1-.367-.367v-15.65a.368.368,0,0,1,.367-.367h6.708a.367.367,0,0,1,.366.367Z"
+              transform="translate(-333.719 -206.326)"/>
+        <g transform="translate(12.297 8.944)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(4.471 8.944)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(4.471 13.415)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(4.471 17.887)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(29.065 8.944)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(29.065 13.415)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(29.065 17.887)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(16.768 8.944)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(21.24 8.944)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(12.297 13.415)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(16.768 13.415)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(16.768 17.887)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(21.24 13.415)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(12.297 4.472)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(16.768 4.472)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+        <g transform="translate(21.24 4.472)">
+          <rect class="c" width="1.502" height="1.502"/>
+        </g>
+      </g>
+    </g>
+  </g>
+</template>
+
+<script>
+  export default {
+    name: 'CsiIconStructureGroup',
+  }
+</script>
+
+
+<style scoped lang="stylus">
+  .csi-icon-structure-group
+    .a {
+      fill: #73d7ff;
+    }
+
+    .b {
+      fill: #fff;
+    }
+
+    .c {
+      fill: #003575;
+    }
+</style>
